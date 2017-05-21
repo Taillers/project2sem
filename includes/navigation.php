df3260f194896732b6ba5b898a505f0a412d8abd
Eric Cocquerez <eric@cocquerez.com>
2017-05-21 19:49:42 +0200

    ajout d'un bouton dans la barre de navigation pour réinitialiser le formulaire Ajout d'une classe pour nettoyer la session

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,6 +2,7 @@
   <div class="container-fluid">
     <ul class="nav navbar-nav">
       <li class="active"><a href="index.php">Accueil</a></li>
+      <li ><a href="clean.php">R.A.Z.</a></li>
       <li><a href="#">Administration</a></li>
     </ul>
   </div>
