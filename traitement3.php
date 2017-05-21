083e5100ff8bf8808dc5adc9d6a2fca2284f505c
Eric Cocquerez <eric@cocquerez.com>
2017-05-21 19:51:07 +0200

    debut sauvegarde des informations de la page 2

--- traitement3.php
+++ traitement3.php
@@ -4,7 +4,14 @@
     {
         $_SESSION['departement'] = $_POST['departement'];
     }
-
+    if(isset($_POST['anciennete']))
+    {
+        $_SESSION['anciennete'] = $_POST['anciennete'];
+    }
+    if(isset($_POST['anneanciennete']))
+    {
+        $_SESSION['anneanciennete'] = $_POST['anneanciennete'];
+    }
     if(isset($_POST['categorie']))    
     {
         $_SESSION['categorie'] = $_POST['categorie'];
