62aa646476b61b1a0dbbcd8c0efa8f7f8340e438
Eric COCQUEREZ <e.cocquerez@netia.com>
2017-05-23 12:32:54 +0200

    ajout du début de connexion

---
+++ connect.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Projet Alexandre Cocquerez</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+  <script src="jquery/jquery-3.2.1.min.js"></script>
+  <script src="css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/perso.css">
+  
+</head>
+<body>
+  <?php include("includes/header.php");?>
+  <?php include("includes/navigation.php");?>
+  <?php 
+    if(isset($_SESSION['connected']) && $_SESSION['connected'] == true;)
+    {
+        session_unset();
+        session_destroy();
+        header("Location: index.php");
+        exit;
+    }
+  ?>
+  <form class="form-inline" method="post" action="admin.php">
+  <div class="form-group">
+    <label for="UserName">Nom d'utilsateur</label>
+    <input type="text" class="form-control" id="UserName" placeholder="admin">
+  </div>
+  <div class="form-group">
+    <label for="userPassword">Mot de passe</label>
+    <input type="password" class="form-control" id="userPassword" placeholder="a43w54j2">
+  </div>
+  <button type="submit" class="btn btn-default">Login</button>
+</form>
+  <?php include("includes/footer.php");?>
+</body>
+</html>
--- includes/navigation.php
+++ includes/navigation.php
@@ -3,7 +3,16 @@
     <ul class="nav navbar-nav">
       <li class="active"><a href="index.php">Accueil</a></li>
       <li ><a href="clean.php">R.A.Z.</a></li>
-      <li><a href="#">Administration</a></li>
+      <?php 
+        if(isset($_SESSION['connected']) && $_SESSION['connected'] == true;)
+        {
+          echo '<li><a href="connect.php">Deconnexion</a></li>';
+        }
+        else
+        {
+          echo '<li><a href="connect.php">Administration</a></li>';
+        }
+      ?>
     </ul>
   </div>
 </nav>
