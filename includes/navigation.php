39b3ea3f366a25fbfe2c3e923b01760cac573bdd
Eric COCQUEREZ <e.cocquerez@netia.com>
2017-05-23 12:42:33 +0200

    correction navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,7 +4,7 @@
       <li class="active"><a href="index.php">Accueil</a></li>
       <li ><a href="clean.php">R.A.Z.</a></li>
       <?php 
-        if(isset($_SESSION['connected']) && $_SESSION['connected'] == true;)
+        if(isset($_SESSION['connected']) && $_SESSION['connected'] == true)
         {
           echo '<li><a href="connect.php">Deconnexion</a></li>';
         }
